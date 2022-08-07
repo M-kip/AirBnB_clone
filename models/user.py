523f2c9aab1b294441e31e4603c9dc84cb5ec11e
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-08-07 21:00:26 +0300

    User class

---
+++ models/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module implements the user class
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+        User class inherits from BaseModel class
+
+        Attributes
+        ----------
+        email: str
+            Email of the user
+        password: str
+            user password
+        first_name: str
+            first name of the user
+        last_name: str
+            last name of the user
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
