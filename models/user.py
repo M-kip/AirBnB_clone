fd8b0139b817e712443f01df443a21d482c078bc
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-08-07 21:12:36 +0300

    user class update

--- models/user.py
+++ models/user.py
@@ -26,3 +26,32 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize Super class and current class
+
+        Parameters
+        ----------
+        args: list
+            List of arguments
+        kwargs: dict
+            Dict of args
+        """
+
+        super().__init__(kwargs)
+        storage.new(self.to_dict)
+
+    def to_dict(self):
+        """
+        Overrides the base class to_dict method and
+        adds User class atrributes
+        """
+
+        dictionary = super().to_dict()
+        dictionary[User.email] = User.email
+        dictionary[User.password] = User.password
+        dictionary[User.first_name] = User.first_name
+        dictionary[User.last_name] = User.last_name
+
+        return dictionary
