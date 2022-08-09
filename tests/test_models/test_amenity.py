c41be590521a195ce5a19bd5da76261cab7c8944
Moses Kiprono <bettmoseskiprono@gmail.com>
2022-08-09 09:44:17 +0300

    updates city & amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,59 +1,93 @@
 #!/usr/bin/python3
-"""Unittest module for the Amenity Class.
+"""
+Unittest module for the Amenity Class.
 """
 
 import unittest
-from datetime import datetime
-import time
+import pycodestyle
 from models.amenity import Amenity
-import re
-import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
+from models import amenity
 from models.base_model import BaseModel
+
+
+class TestAmenityDocs(unittest.TestCase):
+    """
+    Implements tests for the base class
+
+    The test checks if the base_model file conforms to pep8
+    and the correct documentation
+
+    Methods
+    -------
+    test_base_model(self)
+        Run pycodestyle tests
+    """
+
+    def test_amenity_model(self):
+        """ checks if the base_model file conforms to pep8 style """
+        style = pycodestyle.Checker("models/amenity.py", show_source=True)
+        results = style.check_all()
+        self.assertEqual(results, 0, "Found PEP 8 error and Warnings")
+
+    def test_amenity_model_test(self):
+        """ Tests this file for PEP8 errors"""
+
+        style = pycodestyle.Checker("tests/test_models/test_amenity.py",
+                                    show_source=True)
+        results = style.check_all()
+        self.assertEqual(results, 0, "Found PEP 8 errors & Warnings")
+
+    def test_module_doc_string(self):
+        """ Checks if the doc string is present"""
+
+        self.assertIsNot(amenity.__doc__, None,
+                         "amenity.py needs documentation")
+        self.assertTrue(len(amenity.__doc__) >= 1,
+                        "amenity.py need doc string")
+
+    def test_class_doc_string(self):
+        """ Tests if the class doc string exists"""
+
+        self.assertIsNot(Amenity.__doc__, None,
+                         "Amenity class needs doc str")
+        self.assertTrue(len(Amenity.__doc__) >= 1,
+                        "Amenity class needs doc str")
 
 
 class TestAmenity(unittest.TestCase):
 
-    """Test Cases for the Amenity class.
+    """
+    Test Cases for the Amenity class.
+
+    Methods
+    -------
+    test_amenity_instantiation
+        Checks for correctness in initialization
+    test_amenity_attributes
+        checks for attributes correctness
     """
 
-    def setUp(self):
-        """Sets up test methods.
+    def test_amenity_instantiation(self):
         """
-        pass
-
-    def tearDown(self):
-        """Tears down test methods.
-        """
-        self.resetStorage()
-        pass
-
-    def resetStorage(self):
-        """Resets FileStorage data.
-        """
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
-
-    def test_8_instantiation(self):
-        """Tests instantiation of Amenity class.
+        Tests instantiation of Amenity class.
         """
 
-        b = Amenity()
-        self.assertEqual(str(type(b)), "<class 'models.amenity.Amenity'>")
-        self.assertIsInstance(b, Amenity)
-        self.assertTrue(issubclass(type(b), BaseModel))
+        my_amenity = Amenity()
+        self.assertEqual(str(type(my_amenity)),
+                         "<class 'models.amenity.Amenity'>")
+        self.assertIsInstance(my_amenity, Amenity)
+        self.assertTrue(issubclass(type(my_amenity), BaseModel))
 
-    def test_8_attributes(self):
-        """Tests the attributes of Amenity class.
+    def test_amenity_attributes(self):
         """
-        attributes = storage.attributes()["Amenity"]
-        o = Amenity()
+        Tests the attributes of Amenity class.
+        """
+
+        my_amenity = Amenity()
+        attributes = my_amenity.to_dict()
         for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+            self.assertTrue(hasattr(my_amenity, k))
+            self.assertEqual(my_amenity.__dict__[k], v)
 
 
 if __name__ == "__main__":
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -3,57 +3,87 @@
 """
 
 import unittest
-from datetime import datetime
-import time
+import pycodestyle
 from models.city import City
-import re
-import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
+from models import city
 from models.base_model import BaseModel
 
 
-class TestCity(unittest.TestCase):
+class TestCityDocs(unittest.TestCase):
+    """
+    Runs Doc string and pycodestyle checks
 
-    """Test Cases for the City class.
+    This class checks for proper code documentation
+    if the class/ module contains __doc__ strings
+
+    Methods
+    -------
+    test_place_pep8(self)
+        Run checks with pycodestyle
+    test_place_module_doc_string(self)
+        Checks for module doc string
+    test_place_class_doc_string(self)
+        Checks for class doc strings
+    test_place_test_pep8(self)
+        Tests this script for pep compliance
     """
 
-    def setUp(self):
-        """Sets up test methods.
+    def test_place_pep8(self):
         """
-        pass
-
-    def tearDown(self):
-        """Tears down test methods.
+        Checks for PEP 8 compliance
         """
-        self.resetStorage()
-        pass
+        style = pycodestyle.Checker("models/city.py", show_source=True)
+        results = style.check_all()
+        self.assertEqual(results, 0, "Found PEP 8 Warnings")
 
-    def resetStorage(self):
-        """Resets FileStorage data.
+    def test_place_test_pep8(self):
         """
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+        Checks for PEP 8 compliance
+        """
+        style = pycodestyle.Checker("tests/test_models/test_city.py",
+                                    show_source=True)
+        results = style.check_all()
+        self.assertEqual(results, 0, "Found PEP 8 Warnings")
 
-    def test_8_instantiation(self):
-        """Tests instantiation of City class.
+    def test_module_doc_string(self):
+        """ Checks if the doc string is present"""
+
+        self.assertIsNot(city.__doc__, None, "city.py needs documentation")
+        self.assertTrue(len(city.__doc__) >= 1, "city.py need doc string")
+
+    def test_class_doc_string(self):
+        """ Tests if the class doc string exists"""
+
+        self.assertIsNot(City.__doc__, None, "City class needs doc str")
+        self.assertTrue(len(City.__doc__) >= 1,
+                        "City class needs doc str")
+
+
+class TestCity(unittest.TestCase):
+    """
+    Test Cases for the City class.
+    """
+
+    def test_city_instantiation(self):
+        """
+        Tests instantiation of City class.
         """
 
-        b = City()
-        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
-        self.assertIsInstance(b, City)
-        self.assertTrue(issubclass(type(b), BaseModel))
+        my_city = City()
+        self.assertEqual(str(type(my_city)), "<class 'models.city.City'>")
+        self.assertIsInstance(my_city, City)
+        self.assertTrue(issubclass(type(my_city), BaseModel))
 
-    def test_8_attributes(self):
-        """Tests the attributes of City class.
+    def test_city_attributes(self):
         """
-        attributes = storage.attributes()["City"]
-        o = City()
+        Tests the attributes of City class.
+        """
+
+        my_city = City()
+        attributes = my_city.to_dict()
         for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+            self.assertTrue(hasattr(my_city, k))
+            self.assertEqual(my_city.__dict__[k], v)
 
 
 if __name__ == "__main__":
